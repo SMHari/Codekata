cbf4b3f1171c38dea265286544957778b0f57dad
H@r! <s.hariharancs@gmail.com>
2018-01-25 15:38:00 +0530

    Create odd_even.py

---
+++ Beginner/set-1/odd_even.py
@@ -0,0 +1,10 @@
+def main():
+    #n=int(input("Enter a number to count"))
+    n=int(input())
+    if n%2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+if __name__=='__main__':
+    main()
