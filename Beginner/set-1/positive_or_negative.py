8c0cb82c2dba4deac4410405c84d135b3cc92f6a
H@r! <s.hariharancs@gmail.com>
2018-01-25 15:36:28 +0530

    Create positive_or_negative.py

---
+++ Beginner/set-1/positive_or_negative.py
@@ -0,0 +1,12 @@
+def main():
+    #n=int(input("Enter a number"))
+    n=int(input())
+    if n>0:
+        print("Positive")
+    elif n<0:
+        print("Negative")
+    else:
+        print("zero")
+
+if __name__=='__main__':
+    main()
