8196974051369a93cad8a665c619e5e3fa994810
H@r! <s.hariharancs@gmail.com>
2018-01-25 15:39:42 +0530

    Create vowel_or_not.py

---
+++ Beginner/set-1/vowel_or_not.py
@@ -0,0 +1,12 @@
+def main():
+    vowel=['a','e','i','o','u','A','E','I','O','U']
+    #n=(input("Enter a number to character:"))
+    n=input()
+    if n in vowel:
+        print("Vowel")
+    else:
+        print("not a Vowel")
+
+
+if __name__=='__main__':
+    main()
