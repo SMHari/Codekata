ef12c361151070ecab2de60702764e9553da9423
H@r! <s.hariharancs@gmail.com>
2018-02-05 14:31:07 +0530

    Create prime_bet_intervals.py

---
+++ Beginner/set-2/prime_bet_intervals.py
@@ -0,0 +1,13 @@
+def main():
+    sta,end=input().split(" ")
+    sta=int(sta)
+    end=int(end)
+    for num in range(sta,end+1):
+        if num > 1:
+            for i in range(2,num):
+                if (num%i)==0:
+                    break
+            else:
+                print(num)
+if __name__ == '__main__':
+    main()
